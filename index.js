4747f1dc5210c0d75caf7845d75928a756a329fb
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-03 22:48:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+  if (someValue===43){
+    return 1;
+  }else if (someValue===50 || someValue===34){
+    return 8;
+  }
+}
+
+function distanceFromHqinFeet (someValue){
+distanceFromHqInBlocks (someValue===43){
+  return ((someValue-42)*264);
+} else{
+distanceFromHqInBlocks (someValue===50 || someValue===34){
+  return (someValue-42)*264) 
+}
+}
+
+function distanceTravelledInFeet (num1,num2){ 
+if (num1===43 && num2===48) {
+  return ((num2-num1)*264)
+}else if (num1===50 && num2===60){
+  return ((num2-num1)*264)
+}else if (num1===34 && num2===28) {
+  return ((num2-num1)*264)
+}
+
+function calculatesFarePrice (fare) {
+  if (fare <=400) {
+    return "free"
+  }else if (fare >400 && fare<2000){
+    return "2 cents per foot"
+  }else if (fare >2000 && fare <2500){
+    return "flat fare"
+  } else if (fare >2500) {
+    return "cannot travel that far" 
+  }
+  ]
+  
+    
+  
+  
